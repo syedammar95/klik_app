81d2669018ae1b08b1f07d0bc2f7004612a1ed7f
syedammar95 <ammaruspeaks@gmail.com>
2026-08-01 02:20:49 +0500

    maid.dart

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => CartProvider()),
       ],
       child: ScreenUtilInit(
-        designSize: const Size(360, 690),
+        designSize: const Size(360, 69),
         minTextAdapt: true,
         splitScreenMode: true,
         builder: (context, child) {
